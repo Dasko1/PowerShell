90fc6ede66ceed798c46a1eea0e597ad1078d954
dadaskaloopoulos <dadaskalopoulos@sbgtv.com>
2025-02-14 11:23:38 -0500

    Commit Remove-This.ps1

---
+++ Remove-This.ps1
@@ -0,0 +1,12 @@
+# Removes a file defined in ll.4-5!
+
+# Define the folder path for the file to be removed!
+$folderPath = "C:\temp"
+$fileName = "Remove-This.txt"
+$filePath = Join-Path -Path $folderPath -ChildPath $fileName
+
+# Remove the file!
+Remove-Item -Path $filePath -Force
+
+# Output the result!
+Write-Host "File Removed: $filePath"
