05df453914f6e0b3fb22c0f95b6c2736a9867b28
dadaskaloopoulos <dadaskalopoulos@sbgtv.com>
2025-02-18 19:15:20 -0500

    Commit 2 more files!

---
+++ If-Else.ps1
@@ -0,0 +1,17 @@
+# Define a variable, a number between 0 - 100
+$randomNumber = Get-Random -Minimum 0 -Maximum 101
+
+# If-Else statement
+if ($randomNumber -gt 75) {
+    Write-Output "The number is greater than 75."
+} 
+
+elseif ($randomNumber -gt 25 -and $randomNumber -le 75) {
+    Write-Output "The number is between 25 and 75."
+} 
+
+else {
+    Write-Output "The number is 25 or less."
+}
+
+Write-Output "`nThe number actually is: $randomNumber`n"
