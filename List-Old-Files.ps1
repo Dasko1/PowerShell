f6ed14b73375bc89d1474e3a2188287d7eeda221
dadaskaloopoulos <dadaskalopoulos@sbgtv.com>
2025-02-25 14:40:18 -0500

    Added a message if no files older that Cutoff date found.

--- List-Old-Files.ps1
+++ List-Old-Files.ps1
@@ -2,7 +2,7 @@
 
 # Define the folder path and the cutoff date
 $folderPath = "C:\Users\dadaskalopoulos\OneDrive - Sinclair Broadcast Group, Inc\!Profile\Pictures\Screenshots"
-$cutoffDate = Get-Date "2023-10-13"
+$cutoffDate = Get-Date "2023-10-12"
 
 # Get all files in the folder
 $files = Get-ChildItem -Path $folderPath -File
@@ -10,5 +10,12 @@ $files = Get-ChildItem -Path $folderPath -File
 # Filter files older than the cutoff date
 $oldFiles = $files | Where-Object { $_.LastWriteTime -le $cutoffDate }
 
-# Output the list of old files
-$oldFiles | ForEach-Object { $_.FullName }
+if ($oldFiles.Count -gt 0) {
+    # Output the list of old files
+    $oldFiles | ForEach-Object { $_.FullName }
+}
+
+else {
+    Write-Host "`nNo files were found older than $cutoffDate were found.`n"
+}
+
