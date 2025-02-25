b8fda99124211ee110dc2e14687f9c44d7a44224
dadaskaloopoulos <dadaskalopoulos@sbgtv.com>
2025-02-25 14:26:33 -0500

    Edits of older files.

--- Get-Date.ps1
+++ Get-Date.ps1
@@ -1,4 +1,7 @@
-# Get-Date - of course this will give you the Date + Time; ll.3-4 will just give you the date only.
+# Get-Date - of course this will give you the Date; ll.3-4 will just give you the date only. 6 months ago in ll.6-7.
 
 $todayDate = (Get-Date).ToString("M/d/yyyy")
 Write-Output "`nToday's date is: $todayDate`n"
+
+$sixMonthsAgo = (Get-Date).AddMonths(-6).ToString("M/d/yyyy")
+Write-Output "`nSix months ago was: $sixMonthsAgo`n"
--- List-Old-Files.ps1
+++ List-Old-Files.ps1
@@ -1,14 +1,14 @@
-# Lists files older than a certain date. The next file will remove files older than the certain date.  
+# Lists files older than a certain date. The next script will remove files older than the certain date.  
 
 # Define the folder path and the cutoff date
 $folderPath = "C:\Users\dadaskalopoulos\OneDrive - Sinclair Broadcast Group, Inc\!Profile\Pictures\Screenshots"
-$cutoffDate = Get-Date "2023-10-10"
+$cutoffDate = Get-Date "2023-10-13"
 
 # Get all files in the folder
 $files = Get-ChildItem -Path $folderPath -File
 
 # Filter files older than the cutoff date
-$oldFiles = $files | Where-Object { $_.LastWriteTime -lt $cutoffDate }
+$oldFiles = $files | Where-Object { $_.LastWriteTime -le $cutoffDate }
 
 # Output the list of old files
 $oldFiles | ForEach-Object { $_.FullName }
